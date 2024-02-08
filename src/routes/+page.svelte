fd598e630658570c978dd23f4456e0b342b0aaf9
matthwfrost <matthwfrost@gmail.com>
2024-02-08 20:40:16 +0000

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<body>
+    <h1>Privacy Policy for Edel</h1>
+    <p><strong>Effective date:</strong> 08/02/2024</p>
+    <p>This Privacy Policy describes how Edel ("we", "our", or "us") collects, uses, and protects your personal information when you use our services. We are committed to ensuring the privacy and security of your information and handling it in accordance with our privacy practices outlined below.</p>
+    <h2>Information Collection and Use</h2>
+    <p>When you interact with our services, we collect the following personal information from you:</p>
+    <ul>
+        <li><strong>Profile Image:</strong> To help us keep track of the characters you have used.</li>
+        <li><strong>User ID:</strong> Assigned to you for identification and tracking purposes within our services.</li>
+    </ul>
+    <p>This information is used solely for the purpose of tracking the characters used by you and is not used for any other purpose.</p>
+    <h2>Data Sharing and Third-Party Disclosure</h2>
+    <p>We do not share your personal information (profile images and user IDs) with any third parties.</p>
+    <h2>User Rights</h2>
+    <p>You have the right to access your personal information that we hold. However, it is important to note that users cannot delete their user ID. For modifications or questions regarding your personal information, please contact us using the details provided below.</p>
+    <h2>Data Protection</h2>
+    <p>We take the security of your personal information seriously and employ secure servers to protect the information we collect from you.</p>
+    <h2>International Privacy Regulations</h2>
+    <p>Edel does not specifically comply with international privacy regulations such as GDPR or CCPA. However, we are committed to protecting user privacy and ensuring the secure handling of personal information.</p>
+    <h2>Contact Information</h2>
+    <p>If you have any questions or concerns about this Privacy Policy or the handling of your personal information, please contact us via email at <a href="mailto:matthwfrost@gmail.com">matthwfrost@gmail.com</a>.</p>
+
+
+</body>
+<style>
+    h1 {
+        font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,"Helvetica Neue",sans-serif;
+    }
+</style>
