92f2047ee70d6ac3b60500edfd45df07c9cf83fc
matthwfrost <matthwfrost@gmail.com>
2024-04-30 19:20:22 +0100

    play ground added

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,55 @@
+<script></script>
+
+<main class="h-[100vh] flex justify-center items-center font-sans">
+    <article class="w-[50vw]">
+        <h1 class="text-6xl font-bold">Lots of text content</h1>
+        <p class="text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            error ratione ducimus autem doloribus architecto nostrum repudiandae
+            reiciendis. Accusantium iure architecto molestias possimus adipisci
+            minus ea fuga quaerat placeat! Modi.
+        </p>
+        <p class="text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            error ratione ducimus autem doloribus architecto nostrum repudiandae
+            reiciendis. Accusantium iure architecto molestias possimus adipisci
+            minus ea fuga quaerat placeat! Modi.
+        </p>
+        <p class="text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            incidunt minima doloremque ipsa ea deleniti nihil. Debitis, porro
+            quisquam provident soluta maiores voluptatibus similique odit,
+            consectetur voluptas rerum nulla ipsum.
+        </p>
+        <ul>
+            <li>
+                List number 1
+            </li>
+            <li>
+                List number 2
+            </li>
+            <li>
+                List number 3
+            </li>
+            <li>
+                List number 4
+            </li>
+            <li>
+                List number 5
+            </li>
+        </ul>
+    </article>
+</main>
+
+<style>
+    h1 {
+        color: #61d095;
+    }
+    main {
+        background-color: #2a4747;
+    }
+    p {
+        color: #48bf84;
+        margin: 10px;
+    }
+</style>
